44f6bfe1b9509e9ab8a46c0fff0ebb9ebebf7238
raqib03 <72424440+raqib03@users.noreply.github.com>
2023-08-03 21:25:41 +0800

    a layer with dot product using numpy

--- codes/p3.py
+++ codes/p3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
 
 weights = [[0.2, 0.8, -0.5, 1.0],
@@ -6,12 +8,6 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-layer_outputs = [] # output of the current layer
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input*weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
+layer_outputs = np.dot(weights, inputs) + biases
 
 print(layer_outputs)
