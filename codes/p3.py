0db183a5d70c6c0a869e4334f7fcadd940bcc83d
raqib03 <72424440+raqib03@users.noreply.github.com>
2023-08-03 12:32:00 +0800

    without print

--- codes/p3.py
+++ codes/p3.py
@@ -9,17 +9,9 @@ biases = [2, 3, 0.5]
 layer_outputs = [] # output of the current layer
 for neuron_weights, neuron_bias in zip(weights, biases):
     neuron_output = 0
-    print(f'neuron_weights {neuron_weights}')
-    print(f'neuron_bias {neuron_bias}')
     for n_input, weight in zip(inputs, neuron_weights):
-        print(f'n_input {n_input}')
-        print(f'weight {weight}')
         neuron_output += n_input*weight
-        print(f'neuron_output {neuron_output}')
     neuron_output += neuron_bias
-    print(f'neuron_output {neuron_output}')
     layer_outputs.append(neuron_output)
-    print(f'layer_outputs {layer_outputs}')
-
 
 print(layer_outputs)
