e7d44c32f38774f4950ac75a0661036987b2cc8c
raqib03 <72424440+raqib03@users.noreply.github.com>
2023-08-08 20:43:26 +0800

    py4 checking main

--- codes/p4.py
+++ codes/p4.py
@@ -1,23 +1,7 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
-
-weights1 = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-biases1 = [2, 3, 0.5]
-
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5, 0.12, -0.33],
-           [-0.44, -0.73, -0.13]]
-biases2 = [-1, 2, -0.5]
+X = [[1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]]
 
 
-layer1_outputs = np.dot(inputs, np.array(weights1).T) + biases1
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-
-
-print(layer1_outputs)
-print(layer2_outputs)
