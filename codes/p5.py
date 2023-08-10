6a2f127f4a3e878d699e49d5896df65c20b64af9
raqib03 <72424440+raqib03@users.noreply.github.com>
2023-08-10 19:39:19 +0800

    ReLU activated

--- codes/p5.py
+++ codes/p5.py
@@ -1,6 +1,8 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
 X = [[1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
@@ -14,6 +16,8 @@ X = [[1, 2, 3, 2.5],
 
 # print(output)
 
+X, y = spiral_data(100, 3)
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -26,12 +30,12 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+layer1 = Layer_Dense(2, 5)
+activation1= Activation_ReLU()
 
 layer1.forward(X)
 print(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
 
 
-layer2.forward(layer1.output)
-print(layer2.output)
